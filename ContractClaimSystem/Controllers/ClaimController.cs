2fb32e17f5f3edbb4c79a4648993ae64e6477bd9
Boineelo Mkansie <agosimakofane@gmail.com>
2025-09-09 22:36:35 +0200

    Initialize ContractClaimSystem project

---
+++ ContractClaimSystem/Controllers/ClaimController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class ClaimController : Controller
+{
+    public IActionResult Home()
+    {
+        return View();
+    }
+
+    public IActionResult SubmitClaim()
+    {
+        return View();
+    }
+
+    public IActionResult UploadDocuments()
+    {
+        return View();
+    }
+
+    public IActionResult TrackStatus()
+    {
+        return View();
+    }
+
+    public IActionResult VerifyApprove()
+    {
+        return View();
+    }
+}
